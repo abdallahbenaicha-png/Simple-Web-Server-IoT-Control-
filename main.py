91f1e74d1962b5ee9bd89740e60682b66af0c0b9
abdallahbenaicha-png <abdallah.benaicha@ipsas.tn>
2026-06-06 10:27:41 +0100

    Add simple HTTP server for Raspberry Pi IoT

---
+++ main.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(b"<h1>Hello Raspberry Pi IoT</h1>")
+
+server = HTTPServer(("0.0.0.0", 8080), MyHandler)
+
+print("Server running on port 8080...")
+server.serve_forever()
